9b29ac8999d1c47cc9b6ab94de3e7d9310d96479
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-05-04 11:47:07 -0700

    added description and steps

--- easy-challenges/two-sum.js
+++ easy-challenges/two-sum.js
@@ -1,7 +1,14 @@
+//Two Sum
+
 //create object (hashtable) for containing key-value pairs of the element and index
 // iterate through the array
 // for the currentElement, compute the currentDiffference (difference of target - currentElement)
 // if the currentDifference exists in the hashTable, return the index of the currentElement and currentDifference  
+
+//LeetCode - Problem 1
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//You can return the answer in any order.
 
 function twoSum(nums, target){
   numsIndexes = {};
