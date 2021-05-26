b91c4a9373a0ddb35c9f02cfcd6a288f8ca65429
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-05-26 11:22:42 -0700

    square root challenge complete

---
+++ easy-challenges/square-root.js
@@ -0,0 +1,38 @@
+//Given a non-negative integer x, compute and return the square root of x.
+//Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+//Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+
+function mySqrt(x) {
+
+  if(x <= 1){
+    return x;
+  }
+  let start = 1;
+  let end = x/2;
+  while(start <= end){
+    let mid = Math.floor((start + end) / 2);
+    if((mid * mid) <= x && ((mid + 1) * (mid + 1) > x)){
+      return mid;
+    } else if (mid * mid > x){
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  // if(x === 0){
+  //   return 0;
+  // }
+  // if(x === 1){
+  //   return 1;
+  // }
+
+  // let half = Math.floor(x/2);
+  // for(let i = half; i > 0; i--){
+  //   if((i * i) <= x){
+  //     return i;
+  //   }
+  // }
+}
+
+console.log(mySqrt(9));
