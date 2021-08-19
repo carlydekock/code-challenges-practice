24f22199e0dd59813389d58e260f659c6e8bc17e
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-08-18 21:49:05 -0700

    binary tree preorder traversal complete

---
+++ easy-challenges/bt-preorder-traversal.js
@@ -0,0 +1,25 @@
+//Given the root of a binary tree, return the preorder traversal of its nodes values
+//Preorder: current -> left -> right
+
+function preorderTraversal(root){
+  if(root === null){
+    return [];
+  }
+
+  let nodes = [];
+
+  const preOrder = (node) => {
+    nodes.push(node.value);
+
+    if(node.left){
+      preOrder(node.left);
+    }
+
+    if(node.right){
+      preOrder(node.right);
+    }
+  }
+
+  preOrder(root);
+  return nodes;
+}
