3bc95e304fa193cde2a91f4b93e253a419c6c5c8
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-08-19 18:45:33 -0700

    first bad version challenge complete

---
+++ easy-challenges/first-bad-version.js
@@ -0,0 +1,35 @@
+//You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+//Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+//You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+//Example: n = 5, bad = 4
+//Output: 4
+//Explanation:
+//call isBadVersion(3) -> false
+//call isBadVersion(5) -> true
+//call isBadVersion(4) -> true
+//Then 4 is the firs bad version
+
+var solution = function(isBadVersion) {
+  /**
+  //  * @param {integer} n Total versions
+  //  * @return {integer} The first bad version
+   */
+  return function(n) {
+      if(n === 1){
+          return n;
+      }
+      
+      let left = 1;
+      let right = n;
+      while(left < right){
+          let mid = Math.floor((left + right) / 2);
+          let result = isBadVersion(mid);
+          if(result){
+              right = mid;
+          } else {
+              left = mid + 1;
+          }
+      }
+      return right;
+  };
+};
