eed1ecf0c92a690b78986c8c2fd09d56710f9294
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-08-26 16:50:08 -0700

    fizz buzz complete

---
+++ easy-challenges/fizz-buzz.js
@@ -0,0 +1,24 @@
+//Fizz Buzz
+//Given an integer n, return a string array answer (1-indexed) where:
+//answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+//answer[i] == "Fizz" if i is divisible by 3.
+//answer[i] == "Buzz" if i is divisible by 5.
+//answer[i] == i if non of the above conditions are true.
+
+function fizzBuzz(n){
+  let results = [];
+  for(let i = 1; i <= n; i++){
+    if(i % 15 === 0){
+      results.push('FizzBuzz');
+    } else if(i % 5 === 0){
+      results.push('Buzz');
+    } else if(i % 3 === 0){
+      results.push('Fizz');
+    } else {
+      results.push(i.toString());
+    }
+  }
+  return results;
+}
+
+console.log(fizzBuzz(5));
