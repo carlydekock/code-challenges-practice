c58dcea59e8bf0ef8b2dd6cdcb65fd8c92a42871
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-09-06 14:15:48 -0700

    complete in a slightly different way with dfs helper function

--- medium-challenges/number-of-islands.js
+++ medium-challenges/number-of-islands.js
@@ -2,6 +2,7 @@
 //Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
 //An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 //Youtube tutorial: https://www.youtube.com/watch?v=cFnUDdi6DeE&ab_channel=TerribleWhiteboard
+//Tutorial on DFS in 2D arrays: https://www.youtube.com/watch?v=LVlihRYfVVw&ab_channel=MattYang-AlgorithmsPrep%26More
 
 //Going to loop through the grid (nested for loops to iterate over each entry)
 //Checking for a 1
@@ -9,38 +10,35 @@
 
 
 function numIslands(grid){
-
-  const getIslandCount = (grid, i, j) => {
-    if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] === '0'){
-      return 0;
-    }
+  let countOfIslands = 0;
   
-    grid[i][j] = '0';
-  
-    getIslandCount(grid, i+1, j);
-    getIslandCount(grid, i-1, j);
-    getIslandCount(grid, i, j+1);
-    getIslandCount(grid, i, j-1);
-  
-    return 1;
-  };
-
-  if(grid === null || grid.length === 0){
-    return 0;
-  }
-
-  let numberOfIslands = 0;
-
   for(let i = 0; i < grid.length; i++){
     for(let j = 0; j < grid[i].length; j++){
-      let currentNum = grid[i][j];
-      if(currentNum === '1'){
-        numberOfIslands += getIslandCount(grid, i, j);
+      let currentCell = grid[i][j];
+      if(currentCell === '1'){
+        dfsCell(grid, i, j);
+        countOfIslands++;
       }
     }
   }
-  return numberOfIslands;
+  return countOfIslands;
 }
+
+//helper function for dfs on a cell in grid
+const dfsCell = (grid, i, j) => {
+  //check necessary conditions
+  if(i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] === '0'){
+    return;
+  }
+  //process cell
+  grid[i][j] = '0';
+
+  //call dfs as needed
+  dfsCell(grid, i+1, j);
+  dfsCell(grid, i-1, j);
+  dfsCell(grid, i, j+1);
+  dfsCell(grid, i, j-1);
+};
 
 console.log(numIslands([
   ['1','1','1','1','0'],
@@ -55,3 +53,38 @@ console.log(numIslands([
   ['0','0','1','0','0'],
   ['0','0','0','1','1'],
 ]));
+
+
+// function numIslands(grid){
+
+//   const getIslandCount = (grid, i, j) => {
+//     if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] === '0'){
+//       return 0;
+//     }
+  
+//     grid[i][j] = '0';
+  
+//     getIslandCount(grid, i+1, j);
+//     getIslandCount(grid, i-1, j);
+//     getIslandCount(grid, i, j+1);
+//     getIslandCount(grid, i, j-1);
+  
+//     return 1;
+//   };
+
+//   if(grid === null || grid.length === 0){
+//     return 0;
+//   }
+
+//   let numberOfIslands = 0;
+
+//   for(let i = 0; i < grid.length; i++){
+//     for(let j = 0; j < grid[i].length; j++){
+//       let currentNum = grid[i][j];
+//       if(currentNum === '1'){
+//         numberOfIslands += getIslandCount(grid, i, j);
+//       }
+//     }
+//   }
+//   return numberOfIslands;
+// }
