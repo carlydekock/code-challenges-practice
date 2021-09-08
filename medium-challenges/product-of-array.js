87965a1068e1ea96392beee30af1deefeeed6024
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-09-08 12:43:27 -0700

    notes added for reference

--- medium-challenges/product-of-array.js
+++ medium-challenges/product-of-array.js
@@ -28,7 +28,7 @@ function productExceptSelf(nums){
   }
 
   for(let k = 0; k < nums.length; k++){
-    answer[k] = parseInt(leftProduct[k] * rightProduct[k]);
+    answer[k] = parseInt(leftProduct[k] * rightProduct[k]); //using parseInt() to avoid -0s 
   }
 
   return answer;
@@ -36,3 +36,6 @@ function productExceptSelf(nums){
 
 console.log(productExceptSelf([1,2,3,4]));
 console.log(productExceptSelf([-1,1,0,-3,3]));
+
+//Summary: Return an answer array of the products of all numbers such that answer[i] is the product of all numbers except nums[i]. Must write an algorithm that runs in O(n) time and without using the division operation, thus can only pass through the array one time. We'll create two additional array to store products, one from the left side of each nums[i], and one from the right side of each nums[i]. Then, we can multiple the numbers at each index of these two arrays to get to our final product for our answer array. Thinking of each product as all the numbers to the right and to the left, but if we can only pass once, we need to store both of these as we go through the array. Little tricky with Javascript in the end as we can output +/- O. Used parseInt() to take care of what was outputting as -0.
+//Pattern: Need products from left and right, and we know can only do it in O(n) time, so need store up to that point from left and up to that point from right at every index. Then, multiply the values together for the final answer array. Using two loops, going from one side of the array and the other going from the opposite side of the array.
