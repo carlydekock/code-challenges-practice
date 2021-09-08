51e51f616678e6451690d1da61df6e3ea9ec5e24
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-09-08 11:22:19 -0700

    added summary, pattern, and big 0 notes

--- easy-challenges/buy-sell-stock.js
+++ easy-challenges/buy-sell-stock.js
@@ -11,13 +11,16 @@ function maxProfit(prices){
     return 0;
   }
 
-  let buyPrice = prices[0];
-  let maxProfit = 0;
+  //starting with buy price as first day
+  let buyPrice = prices[0]; 
+  let maxProfit = 0; 
 
   for(let i = 0; i < prices.length; i++){
     let currentDayPrice = prices[i];
+    //if currentDay is less than buyPrice, reassign buyPrice
     if(currentDayPrice < buyPrice){
       buyPrice = currentDayPrice;
+      //if maxProfit is less than the currentDay-buyPrice, reassign maxProfit
     } else if((currentDayPrice - buyPrice) > maxProfit){
       maxProfit = currentDayPrice - buyPrice;
     } 
@@ -28,3 +31,7 @@ function maxProfit(prices){
 
 console.log(maxProfit([7,1,5,3,6,4]));
 console.log(maxProfit([7,6,4,3,1]));
+
+//Summary: Starting with the first day as the buyPrice and maxProfit at 0, iterate through the array of prices, continuously calculating the currentDayPrice. If the currentDayPrice is less than the buyPrice, reassign buyPrice. And if the currentDayPrice - buyPrice is greater than the current maxProfit, reassign maxProfit. We'll iterate through the array only once, an return maxProfit at the end.
+//Pattern: Sort of a sliding window, setting up the values for the first ones in the array, and a maxProfit of 0. If prices drop, reassign the buyPrice since we want to buy as low as we can. Calculate the maxProfit each day according to currentDayPrice - buyPrice, if its greater than the current maxProfit then update maxProfit to be the new value.
+//Big O: O(n) iterating through the array one time, space: O(1) constant - storing buyPrice and maxProfit
