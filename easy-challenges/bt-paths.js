82616ef68502e1c566cb288e4451da86b897ba94
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-10-07 11:48:53 -0700

    notes complete

--- easy-challenges/bt-paths.js
+++ easy-challenges/bt-paths.js
@@ -1,8 +1,6 @@
 //257. Binary Tree Paths
 //Given the root of a binary tree, return all root-to-leaf paths in any order.
 //A leaf is a node with no children.
-//taking current node's value -> then moving to children
-//add string that represents the path to a list
 
 //Start at root, add current node
 //Constantly traverse subtrees if children, adding next child's value to the path
@@ -30,3 +28,6 @@ function dfs(node, currentPath, paths){
     dfs(node.right, currentPath + '->', paths);
   }
 }
+
+//Summary: Starting at the root, adding each of the paths as strings to an array. Using a helper function that takes the current node, current path, and the paths array, adding the current node value to the current path string. If it's a leaf node, then push the current path into the paths array. If it's not a leaf node -> continue traversing the left side and the right side.
+//Pattern: Recursion, DFS.
