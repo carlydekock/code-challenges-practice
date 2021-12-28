bb4b73bd3296320e891ce4be8b86ccaab74dfbfd
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-12-28 12:12:05 -0800

    added notes

--- easy-challenges/find-disappeared-nums.js
+++ easy-challenges/find-disappeared-nums.js
@@ -18,3 +18,6 @@ function findDisappearedNumbers(nums){
 }
 
 console.log(findDisappearedNumbers([4,3,2,7,8,2,3,1]));
+
+//Summary: First, loop through and add all the numbers in nums to a set. Then, loop through again starting at 1, and up to nums.length (key piece to make sure nums.length included), checking if i is in the set. If not, then push it into the result array, as it is one of the values not in nums of 1...n.
+//Pattern: Two loops, first to fill set, and next to check the set against values 1...n. This does use extra space, to do it without, we could mark values as checked at the indexes they should be at. 
